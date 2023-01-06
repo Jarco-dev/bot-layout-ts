b1db7cf707a4d29ac23ffc0342967f3c5187c6e8
Jarco-dev <jarco@familiekers.nl>
2023-01-06 20:52:04 +0100

    Update to new discord.js select menu's

--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -32,10 +32,30 @@ const interactionTypes = {
         builder: "ModalBuilder",
         event: "ModalSubmitInteraction"
     },
-    SelectMenuComponent: {
-        folder: "selectMenuComponents",
-        builder: "SelectMenuBuilder",
-        event: "SelectMenuInteraction"
+    StringSelectMenuComponent: {
+        folder: "stringSelectMenuComponents",
+        builder: "StringSelectMenuBuilder",
+        event: "StringSelectMenuInteraction"
+    },
+    UserSelectMenuComponent: {
+        folder: "userSelectMenuComponents",
+        builder: "UserSelectMenuBuilder",
+        event: "UserSelectMenuInteraction"
+    },
+    RoleSelectMenuComponent: {
+        folder: "roleSelectMenuComponents",
+        builder: "RoleSelectMenuBuilder",
+        event: "RoleSelectMenuInteraction"
+    },
+    MentionableSelectMenuComponent: {
+        folder: "mentionableSelectMenuComponents",
+        builder: "MentionableSelectMenuBuilder",
+        event: "MentionableSelectMenuInteraction"
+    },
+    ChannelSelectMenuComponent: {
+        folder: "channelSelectMenuComponents",
+        builder: "ChannelSelectMenuBuilder",
+        event: "ChannelSelectMenuInteraction"
     },
     UserContextMenuCommand: {
         folder: "userContextMenuCommands",
@@ -60,15 +80,13 @@ const interactionTypes = {
         ];
 
     const handlerDir = path.resolve(
-        __dirname,
-        "..",
-        "..",
+        process.cwd(),
         "src",
         "interactions",
         typeSettings.folder
     );
     if (!fs.existsSync(handlerDir)) {
-        fs.mkdirSync(handlerDir);
+        fs.mkdirSync(handlerDir, { recursive: true });
     }
 
     // Collect (sub)directory
